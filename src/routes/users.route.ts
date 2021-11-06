796b26a034f25998c5456f886dcf9c492691fa58
Luiz009 <luizrmelo3@gmail.com>
2021-11-06 18:51:22 -0300

    Desenvolvendo e testando o método Put e Delete

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,6 +1,5 @@
 import { Router, Response, Request, NextFunction } from "express";
 
-
 const usersRoute = Router();
 
 usersRoute.get('/users', (req, res, next) => {
@@ -18,7 +17,20 @@ usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
 
   console.log(req.body);
 
-  res.status(200).send(newUser);
+  res.status(201).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  const modifierUser = req.body;
+
+  modifierUser.uuid = uuid;
+
+  res.status(200).send({ modifierUser });
+})
+
+usersRoute.delete('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(200);
 });
 
 export default usersRoute;
