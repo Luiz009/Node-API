a2ce692d484f8c2a722464f9ed6ed87b586ad3be
Luiz009 <luizrmelo3@gmail.com>
2021-11-06 17:35:05 -0300

    Desenvolvendo e testando o método post

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from "./routes/users.route";
 
 const app = express();
 
+// Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configuração de Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'sucesso!' });
 });
 
+// Inicialização do Servidor
 app.listen(3000, () => {
   console.log('aplicação executando na porta 3000!');
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,13 +5,20 @@ const usersRoute = Router();
 
 usersRoute.get('/users', (req, res, next) => {
   const users = [{ userName: 'Luiz' }];
-  res.status(200).send({users});
+  res.status(200).send({ users });
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid;
-
   res.status(200).send({ uuid });
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+
+  console.log(req.body);
+
+  res.status(200).send(newUser);
 });
 
 export default usersRoute;
